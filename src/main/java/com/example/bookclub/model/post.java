99b569341291e6eb86c68d6c17d224cf6a0cbaaf
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-23 21:55:18 -0600

    created model for the post

--- src/main/java/com/example/bookclub/model/post.java
+++ src/main/java/com/example/bookclub/model/post.java
@@ -21,6 +21,9 @@ public class post {
     @Column
     private String body;
 
+    @Column
+    private Integer page;
+
 //    https://stackoverflow.com/questions/54840769/how-to-persist-localdate-with-jpa
     @Column(columnDefinition = "DATE")
     private LocalDate date;
@@ -28,6 +31,64 @@ public class post {
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
 
+    public post() {
+    }
 
 
+    public post(Long id, String name, String body, Integer page, LocalDate date, LocalDateTime dateTime) {
+        Id = id;
+        this.name = name;
+        this.body = body;
+        this.page = page;
+        this.date = date;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
 }
