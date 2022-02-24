934c706a43b20155fd0c6ad05f57ab0474d72305
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-24 14:20:59 -0600

    quote service and controller and tested in postman

--- src/main/java/com/example/bookclub/controller/QuoteController.java
+++ src/main/java/com/example/bookclub/controller/QuoteController.java
@@ -1,4 +1,63 @@
 package com.example.bookclub.controller;
 
+import com.example.bookclub.model.Quote;
+import com.example.bookclub.service.QuoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="/quote")
 public class QuoteController {
+
+    @Autowired
+    public QuoteService quoteService;
+
+
+    //get hello world
+    @GetMapping(path = "/hello-world/")
+    public String getHelloWorld(){
+        return "hello world";
+    }
+
+
+
+    //CREATE
+    @PostMapping("")
+    public Quote createQuote (@RequestBody Quote quoteObj){
+        return quoteService.createQuote(quoteObj);
+    }
+    //GET ALL Quote
+    @GetMapping("/quotes/")
+    public List<Quote> getQuoteList(){
+        return quoteService.getQuoteList();
+    }
+
+    //GET ONE Quote
+    @GetMapping("/{quoteId}/")
+    public Optional<Quote> getOneQuote(@PathVariable(value = "quoteId")Long quoteId){
+        return quoteService.getOneQuote(quoteId);
+    }
+
+
+    //UPDATE ONE Quote
+    @PutMapping("/{quoteId}/")
+    public Quote updateQuote(@PathVariable(value = "quoteId")Long quoteId,
+                           @RequestBody Quote quoteObj){
+        return quoteService.updateQuote(quoteId, quoteObj);
+    }
+
+
+
+    //delete one Quote
+    @DeleteMapping("/{quoteId}/")
+    public Optional<Quote> deleteQuote(@PathVariable (value = "quoteId") Long quoteId) {
+
+        return quoteService.deleteQuote(quoteId);
+    }
+
+
+
 }
--- src/main/java/com/example/bookclub/service/QuoteService.java
+++ src/main/java/com/example/bookclub/service/QuoteService.java
@@ -1,4 +1,84 @@
 package com.example.bookclub.service;
 
+import com.example.bookclub.exceptions.InformationNotFoundException;
+import com.example.bookclub.model.Quote;
+import com.example.bookclub.repository.QuoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class QuoteService {
+
+    private QuoteRepository quoteRepository;
+
+    @Autowired
+    public void setQuoteRepository(QuoteRepository quoteRepository){
+        this.quoteRepository = quoteRepository;
+    }
+
+
+    //create quote
+    public Quote createQuote(Quote quoteObj){
+        System.out.println("Service calling create quote method ==> ");
+        return quoteRepository.save(quoteObj);
+    }
+
+    //get quote list
+    public List<Quote> getQuoteList(){
+        System.out.println("Service calling getQuoteList --> ");
+        return quoteRepository.findAll();
+    }
+
+
+//get one post
+
+    public Optional<Quote> getOneQuote(Long quoteId){
+        System.out.println("Service calling getOneQuote ==>");
+        Optional<Quote> quote = quoteRepository.findById(quoteId);
+        if (quote.isPresent()){
+            return quote;
+        }else{
+            throw new InformationNotFoundException("quote with id of " + quoteId +  " not found");
+        }
+    }
+
+    //update one quote
+    public Quote updateQuote(Long quoteId, Quote quoteObj){
+        System.out.println("service calling updateQuote ==> ");
+        Optional<Quote> quote = quoteRepository.findById(quoteId);
+        if(quote.isPresent()) {
+            Quote updateQuote = quoteRepository.findById(quoteId).get();
+            updateQuote.setChapter(quoteObj.getChapter());
+            updateQuote.setQuote(quoteObj.getQuote());
+            updateQuote.setName(quoteObj.getName());
+            updateQuote.setPage(quoteObj.getPage());
+
+
+            return quoteRepository.save(updateQuote);
+        }
+        else {
+            throw new InformationNotFoundException("Quote with id " + quoteId + " not found");
+        }
+
+    }
+
+
+    //delete one quote
+    public Optional<Quote> deleteQuote(Long quoteId){
+        System.out.println("Service calling deleteQuote ==> ");
+        Optional<Quote> quote = quoteRepository.findById(quoteId);
+        if(quote.isPresent()) {
+            quoteRepository.deleteById(quoteId);
+            return quote;
+        }else{
+            throw new InformationNotFoundException("quote with id " + quoteId + " not found");
+        }
+
+    }
+
+
+
 }
