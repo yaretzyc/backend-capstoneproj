ab69cece26c0606ed95fd8980ece48771c78b2db
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-23 21:51:45 -0600

    added the localdatetime

--- src/main/java/com/example/bookclub/model/post.java
+++ src/main/java/com/example/bookclub/model/post.java
@@ -1,4 +1,33 @@
 package com.example.bookclub.model;
 
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "posts")
 public class post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long Id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String body;
+
+//    https://stackoverflow.com/questions/54840769/how-to-persist-localdate-with-jpa
+    @Column(columnDefinition = "DATE")
+    private LocalDate date;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime dateTime;
+
+
+
 }
