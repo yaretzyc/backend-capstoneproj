ec737893e5ca04a93ac3adc0cb89d68ccf784b21
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-24 10:20:46 -0600

    created the getBookList method and created the pgAdmin book database

--- src/main/java/com/example/bookclub/controller/BookController.java
+++ src/main/java/com/example/bookclub/controller/BookController.java
@@ -1,4 +1,49 @@
 package com.example.bookclub.controller;
 
+import com.example.bookclub.model.Book;
+import com.example.bookclub.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/book")
 public class BookController {
+
+    @Autowired
+    public BookService bookService;
+
+    //get hello world
+    @GetMapping(path = "/hello-world/")
+    public String getHelloWorld(){
+        return "hello world";
+    }
+
+    ////////////////////BOOKS \\\\\\\\\\\\\\\\\\\\\
+
+    @PostMapping("book")
+    public Book createBook (@RequestBody Book bookObj){
+        return bookService.createBook(bookObj);
+    }
+
+    public List<Book> getBookList(){
+        return bookService.getBookList();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/bookclub/service/BookService.java
+++ src/main/java/com/example/bookclub/service/BookService.java
@@ -1,4 +1,33 @@
 package com.example.bookclub.service;
 
+import com.example.bookclub.model.Book;
+import com.example.bookclub.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public Book createBook(Book bookObj){
+        System.out.println("Service calling create book method ==> ");
+//        Book book = bookRepository.
+        return bookRepository.save(bookObj);
+    }
+
+    public List<Book> getBookList(){
+        System.out.println("Service calling getBookList --> ");
+        return bookRepository.findAll();
+    }
+
+
+
+
 }
