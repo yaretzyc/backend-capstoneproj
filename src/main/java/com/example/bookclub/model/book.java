9e70c583580af0281d90d0a2c729ac958d36c73d
Yaretzy Charo <yaretzycharo@my.unt.edu>
2022-02-23 21:26:54 -0600

    model for book

--- src/main/java/com/example/bookclub/model/book.java
+++ src/main/java/com/example/bookclub/model/book.java
@@ -1,4 +1,125 @@
 package com.example.bookclub.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class book {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String author;
+
+    @Column
+    private String img;
+
+    @Column
+    private String summary;
+
+    @Column
+    private String link;
+
+    @Column
+    private String pages;
+
+    @Column
+    private String genre;
+
+    @Column
+    private String other;
+
+
+    public book() {
+    }
+
+    public book(Long id, String title, String author, String img, String summary, String link, String pages, String genre, String other) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.img = img;
+        this.summary = summary;
+        this.link = link;
+        this.pages = pages;
+        this.genre = genre;
+        this.other = other;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
 }
